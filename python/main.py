f50d163f03afe0e472c72661a0428fec391738c1
Nethack <85522443+Net-hacker@users.noreply.github.com>
2026-01-16 15:29:13 +0100

    CODE ÄNDERUNG PARTIELLE
    Daran wird noch gearbeitet

--- python/main.py
+++ python/main.py
@@ -1,7 +1,36 @@
-import cv2
-cam = cv2.VideoCapture(0)
+#import cv2
+import keyboard
+import RPi.GPIO as GPIO
+
+GPIO.setup(18, GPIO.OUT) #Vorwärts
+GPIO.setup(19, GPIO.OUT) #Rückwärts
+GPIO.setup(23, GPIO.OUT) #Rechts
+GPIO.setup(24, GPIO.OUT) #Links
+Move = False
+
+while True:
+    if Move == True:
+        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(24, GPIO.HIGH)
+    else:
+        GPIO.output(23, GPIO.LOW)
+        GPIO.output(24, GPIO.LOW)
+        
+    if keyboard.is_pressed('w'):
+        Move = True
+        GPIO.output(18, GPIO.HIGH)
+
+    if keyboard.is_pressed("a"):
+        print("Links")
+    if keyboard.is_pressed("s"):
+        print("Rück")
+    if keyboard.is_pressed("d"):
+        print("Rechts")
+
+
+'''cam = cv2.VideoCapture(0)
 while cam.isOpened():
     ret, frame = cam.read()
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow("WRO-CAM", frame)
+    cv2.imshow("WRO-CAM", frame)'''
