1fced1663b60a678a18ebf156786b38be9db4850
lauschti <samuel.lausterer@gmail.com>
2026-01-17 18:16:53 +0100

    Update Lenkung
    Lenkung wurde implementiert. Tastatursteuerung ist nun voll Funktionsfähig

--- python/main.py
+++ python/main.py
@@ -4,50 +4,74 @@ import time
 
 from gpiozero import PWMOutputDevice, DigitalOutputDevice
 
-# Pin-Belegung:
+#______________________________________________________________________________________
+# Pin-Belegung (BCM):
 # 18 = LPWM (eine Richtung)
 # 19 = RPWM (andere Richtung)
 # 23 = L_EN
 # 24 = R_EN
+# 12 = Servo
+#_______________________________________________________________________________________
 
-LPWM = PWMOutputDevice(18)       # LPWM
-RPWM = PWMOutputDevice(19)       # RPWM
-L_EN = DigitalOutputDevice(23)   # L_EN
-R_EN = DigitalOutputDevice(24)   # R_EN
+#Motor Setup____________________________________________________________________________
+LPWM = PWMOutputDevice(18)
+RPWM = PWMOutputDevice(19)
+L_EN = DigitalOutputDevice(23)
+R_EN = DigitalOutputDevice(24)
+POWER = 0.3  
 
-POWER = 0.7  # 20 % Leistung
+#Servo Setup____________________________________________________________________________
+SERVO = PWMOutputDevice(12, frequency=65)
+SERVO_LEFT   = 0.047  
+SERVO_CENTER = 0.075  
+SERVO_RIGHT  = 0.1 
+servo_pos = "mitte"
+
 
 Move = False
 last_no_key_time = time.time()
 
+
+def motor_stop():
+    global Move
+    Move = False
+    LPWM.value = 0
+    RPWM.value = 0
+    L_EN.off()
+    R_EN.off()
+
+def servo_set(pos):
+        SERVO.value = pos
+
+servo_set(SERVO_CENTER)
+
+#________________________________________________________________________________________
 while True:
-    if not keyboard.is_pressed('w') and not keyboard.is_pressed('s'):
-        if Move:  
-            pass
+    forward_pressed  = keyboard.is_pressed('w')
+    backward_pressed = keyboard.is_pressed('s')
+
+    # Puffer, bevor Motor stoppt
+    if not forward_pressed and not backward_pressed:
         if time.time() - last_no_key_time > 0.05:  # 50 ms Puffer
-            Move = False
+            motor_stop()
     else:
         last_no_key_time = time.time()
         Move = True
-    
-    if Move:
-        pass
-    else:
-        LPWM.value = 0
-        RPWM.value = 0
-        L_EN.off()
-        R_EN.off()
+    if not Move:
+        motor_stop()
 
-    if keyboard.is_pressed('w'):
+
+    # VORWÄRTS
+    if forward_pressed:
         Move = True
-        # nur RPWM aktiv
-        R_EN.on()
         L_EN.on()
+        R_EN.on()
         RPWM.value = POWER
         LPWM.value = 0
         print("VORWÄRTS")
 
-    if keyboard.is_pressed('s'):
+    # RÜCKWÄRTS
+    if backward_pressed:
         Move = True
         L_EN.on()
         R_EN.on()
@@ -55,23 +79,35 @@ while True:
         RPWM.value = 0
         print("RÜCK")
 
-    if keyboard.is_pressed("a"):
+
+    # LENKUNG
+    if keyboard.is_pressed("a") and servo_pos != "left":
+        servo_set(SERVO_LEFT)
+        servo_pos="left"
         print("LINKS")
 
-    if keyboard.is_pressed("d"):
+    if keyboard.is_pressed("d") and servo_pos != "right":
+        servo_set(SERVO_RIGHT)
+        servo_pos="right"
         print("RECHTS")
 
-    if not keyboard.is_pressed("w") and not keyboard.is_pressed("s"):
-        Move = False
-        LPWM.value = 0
-        RPWM.value = 0
-        L_EN.off()
-        R_EN.off()
+    if not keyboard.is_pressed("a") and not keyboard.is_pressed("d") and servo_pos != "mitte":
+        servo_set(SERVO_CENTER)
+        servo_pos="mitte"
+        print("mitte")
 
+
+    # Programm beenden
     if keyboard.is_pressed("x"):
-        exit()
-    
+        motor_stop()
+        SERVO.value = 0
+        break
+
+    #CPU entlasten
     time.sleep(0.01)
+
+
+
 '''
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
