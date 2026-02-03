d72fb477872f61d7e2c9581c637c72189463e291
Nethack <85522443+Net-hacker@users.noreply.github.com>
2026-02-03 14:22:19 +0100

    ERSTER CAM TEST

--- python/cam.py
+++ python/cam.py
@@ -1,8 +1,24 @@
 import cv2
+import numpy as np
+import sys
+
 cam = cv2.VideoCapture(0)
+if cam == None:
+    print("Kamera konnte nicht geoeffnet werden!")
+    sys.exit(1)
+
 while cam.isOpened():
-    ret, frame = cam.read()
+    _, frame = cam.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    upper_blue = np.array([50, 255, 255])
+    lower_blue = np.array([0, 10, 10])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow("WRO-CAM", frame)
-
+    cv2.imshow("WRO-CAM", mask)
+    cv2.imshow("Test", frame)
