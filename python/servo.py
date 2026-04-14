537fa75cd5b59d72750a8a85b05cf7c55469f14a
lauschti <samuel.lausterer@gmail.com>
2026-04-14 17:33:44 +0200

    servosteuerung

---
+++ python/servo.py
@@ -0,0 +1,10 @@
+from gpiozero import Servo
+import warnings
+import time
+warnings.filterwarnings("ignore")  # Warnung unterdrücken
+
+SERVO = Servo(12, min_pulse_width=1/1000, max_pulse_width=2/1000)
+
+def steer(position: float):
+    SERVO.value = max(-1.0, min(1.0, position))
+
