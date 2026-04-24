d63068dd0032707cccbbf07b34a1aefb37e063b0
Nethack <85522443+Net-hacker@users.noreply.github.com>
2026-04-24 19:15:19 +0200

    ICH HOFFE ES FUNKTIONIERT!
    Also wir haben hier den Kompass um das Auto auf den richtigen Weg zu bringen

--- python/kompass.py
+++ python/kompass.py
@@ -1,22 +1,44 @@
 import board
 import adafruit_bno055
-import kaliBinLaden
+import kaliLaden
 import math
 
 i2c = board.I2C()
 
 sensor = adafruit_bno055.BNO055_I2C(i2c, address=0x29)
 
-def calc(angleM, angleR):
-    return round(math.sin(angleM - angleR))
+def calc(angleM):
+    if angleM > 180:
+        angleNew = angleM - 360
+        if angleNew < -90 || angleNew > 90:
+            print("ES KRACHT!!!")
+            return 0.0, 0.0
+        angleExp1 = angleNew + 90
+    else:
+        angleExp1 = angleM + 90
+    angleExp2 = angleM - 90
+
+    return angleExp1, angleExp2
 
 def direction(angle):
-    Cock = sensor.euler[0]
+    kompass = sensor.euler[0]
 
-    print(f"Penisgrad: {Cock}°")
-    print(calc(angle, Cock))
+    print(f"Grad: {kompass}°")
+    angleExp1, angleExp2 = calc(angle)
+    print(f"Expected: {angleExp1}, {angleExp2}")
 
-kaliBinLaden.load_calibration(sensor)
+    if math.abs(angleExp1 - kompass) > math.abs(angleExp2 - kompass):
+        return 1 # Rechts
+    elif math.abs(angleExp1 - kompass) < math.abs(angleExp2 - kompass):
+        return 0 # Links
+    else:
+        return -1 # ERROR
 
-while True:
-    direction(0)
+# Main Program
+
+kaliLaden.load_calibration(sensor)
+
+for i in range(0, 3):
+    time.sleep(1)
+
+direction(0)
